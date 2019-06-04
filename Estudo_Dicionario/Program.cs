fe09c56c25ae01cc15c7bb5bcc0d6bcee54b5da3
Marcelo Silva <ribmarc@hotmail.com>
2019-06-04 20:18:56 -0300

    Criação da classe Livro

--- Estudo_Dicionario/Program.cs
+++ Estudo_Dicionario/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Estudo_Dicionario
 {
+    public class Livro
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
